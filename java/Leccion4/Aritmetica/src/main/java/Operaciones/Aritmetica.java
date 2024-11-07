13191830b1c44522c5bc57199be3b4813b382c39
Elias Jacob <eliasdavidjacob@gmail.com>
2024-11-07 17:09:51 -0300

    Added new files and updated the project, class 4

---
+++ java/Leccion4/Aritmetica/src/main/java/Operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+
+package Operaciones;
+
+public class Aritmetica {
+    // Atributos de Clase
+    int a; 
+    int b;
+    
+    //Metodo
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("Resultado = " + resultado);
+    
+}
